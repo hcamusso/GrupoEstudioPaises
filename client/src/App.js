9e3d4b61f9b01018e2d443a401002e8a4068ce6d
Alicia Benítez Samaniego <benitez.ali@gmail.com>
2022-07-01 09:33:43 -0300

    app con las rutas

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import LandingPage from '../../client/src/components/LandingPage.jsx';
+import { Route } from 'react-router-dom';
+import Home from '../../client/src/components/Home.jsx';
+import CreateForm from '../../client/src/components/CreateFrom.jsx';
+import CardDetail from '../../client/src/components/CardDetail.jsx';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+    <Route exact path='/home/:id' component={CardDetail}/>
+    <Route exact path='/home' component={Home}/>
+    <Route exact path='/' component={LandingPage}/>
+    <Route exact path='/create' component={CreateForm}/>
     </div>
   );
 }
